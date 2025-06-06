454705a990f6bbfdd3db17c02a1ee8594d7b1832
Zekkruum <juancamilogarciabonilla54@gmail.com>
2025-06-05 21:48:20 -0500

    Update manipulacion_listas.py

--- manipulacion_listas.py
+++ manipulacion_listas.py
@@ -1,7 +1,6 @@
 def multiplos_de_n(lista, n):
     return [numero for numero in lista if numero % n == 0]
 
-# Ejemplo de uso
 numeros = [5, 12, 8, 20, 7]
 n = 4
 resultado = multiplos_de_n(numeros, n)
